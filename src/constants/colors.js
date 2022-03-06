9945e4a8c89f73855970e075d5f9dd9cf7660c7b
MJ <mjkochuk@gmail.com>
2022-03-06 15:27:33 -0700

    Added dbrand logo SVG and styled header bar

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,6 +1,7 @@
 const Colors = Object.freeze({
   White: "#FFFFFF",
   Black: "#000000",
+  DBrandYellow: "#FB0",
 });
 
 export default Colors;
--- src/pages/TitleBar.js
+++ src/pages/TitleBar.js
@@ -1,13 +1,25 @@
 import styled from "styled-components";
+import DBrandLogo from "../Icons/DBrandLogo";
+import Colors from "../constants/colors";
+import FontSizes from "../constants/fontSizes";
 
 const Wrapper = styled.div`
-  height: 30px;
+  height: 50px;
   width: 100%;
-  text-align: center;
+  color: ${Colors.White};
+  background-color: ${Colors.Black};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: ${FontSizes.H3};
 `;
 
 const TitleBar = () => {
-  return <Wrapper>DBrand Crazy Eights</Wrapper>;
+  return (
+    <Wrapper>
+      <DBrandLogo size={20} style={{ marginRight: 10 }} /> Crazy Eights
+    </Wrapper>
+  );
 };
 
 export default TitleBar;
